ac50c34b11777c5f541bb84a9df099c7a7e1dc7d
Rachel Arthur <rarthur06@yahoo.com>
2015-09-15 17:02:20 -0500

    Cleaned up .rmd, added code to TitanicAnalysis.R, Plot 1 code and img

--- 01 Data/Titanic Analysis.R
+++ 01 Data/Titanic Analysis.R
@@ -1,45 +1,106 @@
 require("jsonlite")
 require("RCurl")
-# Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where age >= 40"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
-summary(df)
-head(df)
+# Creates the titanic dataframe
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-require(extrafont)
+# display a subset and summary of the datafram
+head(subset(df, select=c(PCLASS, NAME, FARE)))
+summary(df)
+
+# Create Unique Plot
+ggplot() +
+  coord_cartesian() +
+  scale_x_discrete() +
+  scale_y_continuous() +
+  layer(data=subset(df, !is.na(PCLASS)),
+        mapping=aes(x=PCLASS, y=as.numeric(as.character(FARE))),
+        stat="identity",
+        stat_params=list(),
+        geom="point", geom_params=list(),
+        position=position_jitter(width=0.3,height=0)
+  )
+
+# Create Plot #1
 ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_continuous() +
-  #facet_wrap(~SURVIVED) +
-  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
-  facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Titanic') +
-  labs(x="Age >= 40", y=paste("Fare")) +
+  labs(x="Age", y="Fare") +
   layer(data=df, 
         mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX), 
         stat="identity", 
         stat_params=list(), 
         geom="point",
         geom_params=list(), 
-        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )  
+
+# Create Plot #2
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  labs(title='Titanic') +
+  labs(x="Age", y="Fare") +
+  layer(data=df, 
+        mapping=aes(x=as.numeric(AGE), y=as.numeric(as.character(FARE)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        position=position_jitter(width=0.3, height=0)
+  )  
+
+#Create Plot #3
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  labs(title='Titanic') +
+  labs(x="Survived", y=paste("Fare")) +
+  layer(data=df, 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
         position=position_jitter(width=0.3, height=0)
   )
 
+#Create Plot #4
 ggplot() + 
   coord_cartesian() + 
   scale_x_discrete() +
   scale_y_continuous() +
   facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Titanic') +
-  labs(x="SURVIVED", y=paste("FARE")) +
+  labs(x="Survived", y=paste("Fare")) +
   layer(data=df, 
         mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
         stat="identity", 
         stat_params=list(), 
         geom="point",
         geom_params=list(), 
-        #position=position_identity()
         position=position_jitter(width=0.3, height=0)
   )
 
+# Change SQL statement in creation of dataframe for plot #5
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where age <= 10"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+# Create plot #5
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='Titanic where age <= 10') +
+  labs(x="Survived", y=paste("Fare")) +
+  layer(data=df, 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        position=position_jitter(width=0.3, height=0)
+  )
---
+++ 02 Visualizations/Plot1.R
@@ -0,0 +1,15 @@
+# Create Plot #1
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  labs(title='Titanic') +
+  labs(x="Age", y="Fare") +
+  layer(data=df, 
+        mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        position=position_jitter(width=0.3, height=0)
+  )
